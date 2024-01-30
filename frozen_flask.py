86188de957503ad51417dcf4ec445669f544c35c
Your Name <roger1030142@gmail.com>
2024-01-31 00:14:28 +0800

    更改凍結目錄名稱

--- frozen_flask.py
+++ frozen_flask.py
@@ -2,7 +2,7 @@ from flask_frozen import Freezer
 from app import app  # 從您的 Flask 應用程序導入 app 實例
 
 
-app.config['FREEZER_DESTINATION'] = 'build'  # 指定凍結檔案的目錄
+app.config['FREEZER_DESTINATION'] = 'docs'  # 指定凍結檔案的目錄
 freezer = Freezer(app)  # 創建一個 Freezer 實例
 
 if __name__ == '__main__':
